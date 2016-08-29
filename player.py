e2f033dc939e40cf0c93009e4cd279d969b7afdd
Braidfifteen <raatkin15@gmail.com>
2016-08-29 19:30:54 -0400

    ...

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface([width, height])
         self.image.fill(color)
         self.rect = self.image.get_rect()
+        self.game = game
         self.rect.x = x
         self.rect.y = y
         self.moveX = 0
